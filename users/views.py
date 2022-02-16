9aa5c910859da1f25720de4ec75da19a03342858
JanVanAik <ar.orlovskiy@gmail.com>
2022-02-16 19:13:28 +0300

    registered app, added forms, connected urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from users.forms import UserLoginForm
+
+
+def login(request):
+    form = UserLoginForm()
+    context = {'title': 'Geekshop - Авторизация', 'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {'title': 'Geekshop - Регистрация'}
+    return render(request, 'users/registration.html', context)
