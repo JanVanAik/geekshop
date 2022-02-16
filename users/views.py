04fa90d46a13c77d91a74b1a76dc0a3ab209ac43
JanVanAik <ar.orlovskiy@gmail.com>
2022-02-16 22:49:58 +0300

    logout

--- users/views.py
+++ users/views.py
@@ -31,3 +31,8 @@ def registration(request):
         form = UserRegistrationForm
     context = {'title': 'Geekshop - Регистрация', 'form': form}
     return render(request, 'users/registration.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
